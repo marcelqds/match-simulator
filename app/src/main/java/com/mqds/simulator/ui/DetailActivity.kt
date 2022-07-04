4b821129f784630b401a60a5d58a690992dedcfa
Marcelo Queiroz de Santana <marcelqdstrab@gmail.com>
2022-07-04 20:51:20 -0300

    Create cardView and create detail_activity

---
+++ app/src/main/java/com/mqds/simulator/ui/DetailActivity.kt
@@ -0,0 +1,18 @@
+package com.mqds.simulator.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mqds.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
